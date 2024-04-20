c0c1df73203ce8a0a021af19b764277772ec9d28
siddhesh.palekar@go.ooo <siddhesh.palekar@go.ooo-at-636123530827>
2024-04-20 13:02:44 +0530

    excel

---
+++ Constants/ExcelConstants.ts
@@ -0,0 +1,5 @@
+export default class ExcelConstants {
+    static readonly TEST_PATH = '../Resources/test1.xlsx';
+    static readonly SUITE_PATH = '../Resources/test1.xlsx';
+    static readonly YES = "YES";
+}
